d8abc15f10e76576225f1d324d1a0af7af6a86a4
Adrian Tut <adriantutuianu01@gmail.com>
2024-05-16 22:18:49 +0100

    Moved code from app to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
-import React, { useEffect } from "react";
-import { getCountries } from "./apiService";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import Button from "./Button";
 import "./components/homepage/Home";
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const countriesData = await getCountries();
-        console.log(countriesData);
-      } catch (error) {
-        console.error("Error fetching countries:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleClick = () => {
-    console.log("Button clicked!");
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <div className="flex justify-center items-center h-screen">
-          <Button onClick={handleClick} text="Click me" />
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>TEst</div>;
 }
 
 export default App;
--- src/components/homepage/Home.tsx
+++ src/components/homepage/Home.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getCountries } from "../../apiService";
+import Button from "../../Button";
+
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const countriesData = await getCountries();
+        console.log(countriesData);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleClick = () => {
+    console.log("Button clicked!");
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="flex justify-center items-center h-screen">
+          <Button onClick={handleClick} text="Click me" />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
