3a6c1cd8f02d24d927997d6fc28c196ac3089b97
Adrian Tut <adriantutuianu01@gmail.com>
2024-05-16 22:46:54 +0100

    Added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import "./App.css";
 import Homepage from "./components/homepage/Home";
 import CountryDetails from "./components/CountryDetails/CountryDetails";
 import NotFoundRoute from "./components/NotFoundRoute/NotFoundRoute";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/country/:id" element={<CountryDetails />} />
-        <Route path="*" element={<NotFoundRoute />} />
-      </Routes>
+      <div>
+        <Header /> {/* Assuming Header is your header component */}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/country/:id" element={<CountryDetails />} />
+          <Route path="*" element={<NotFoundRoute />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
