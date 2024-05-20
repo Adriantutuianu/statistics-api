1e90ec9a732eac319a708f0d7d06c25f48b774bc
Adrian Tut <adriantutuianu01@gmail.com>
2024-05-20 22:58:02 +0100

    Removed button header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import Button from "../../Button";
 import "./header.css";
 
 const Header = () => {
-  const handleClick = () => {
-    console.log("Button clicked!");
-  };
-
   return (
     <header className="header">
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
-      <div className="flex justify-center items-center h-screen">
-        <Button onClick={handleClick} text="Click me" />
-      </div>
+      <div className="flex justify-center items-center h-screen"></div>
     </header>
   );
 };
