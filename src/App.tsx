0fbd188d3a96e385eb45f9d2cb285ef5748d23e0
Adrian Tut <adriantutuianu01@gmail.com>
2024-06-24 21:57:22 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/homepage/Home";
-import CountryDetails from "./components/countryDetails/CountryDetails";
+import CountryDetails from "./components/country-details/CountryDetails";
 import NotFoundRoute from "./components/notFoundRoute/NotFoundRoute";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
