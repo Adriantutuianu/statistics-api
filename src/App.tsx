4aa2a9ac48b27a1697a9c5b984fa83ef2c8ae8f6
Adrian Tut <adriantutuianu01@gmail.com>
2024-06-24 22:39:49 +0100

    Added back name to initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Homepage from "./components/homepage/Home";
-import CountryDetails from "./components/country-details/CountryDetails";
-import NotFoundRoute from "./components/not-found-route/NotFoundRoute";
+import Homepage from "./components/Homepage/Home";
+import CountryDetails from "./components/CountryDetails/CountryDetails";
+import NotFoundRoute from "./components/NotFoundRoute/NotFoundRoute";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
