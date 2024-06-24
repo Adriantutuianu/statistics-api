b2a88c7f10e982081b5dfcd7d13e4d3ba78fbb03
Adrian Tut <adriantutuianu01@gmail.com>
2024-06-24 22:22:07 +0100

    Test update

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import "./App.css";
 import Homepage from "./components/homepage/Home";
 import CountryDetails from "./components/country-details/CountryDetails";
 import NotFoundRoute from "./components/not-found-route/NotFoundRoute";
-// import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        {/* <Header /> */}
+        <Header />
         <main className="content">
           <Routes>
             <Route path="/" element={<Homepage />} />
