269ffcd265e6517c00712c583ac68abac0d4a0ef
Adrian Tut <adriantutuianu01@gmail.com>
2024-06-24 22:45:54 +0100

    Pushing all code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/Homepage/Home";
-import CountryDetails from "./components/CountryDetails/CountryDetails";
 import NotFoundRoute from "./components/NotFoundRoute/NotFoundRoute";
+import CountryDetails from "./components/CountryDetails/CountryDetails";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
