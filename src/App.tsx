ecc848ba37ca2dfa8f6d8db0032d34ae64f9bc3a
Adrian Tut <adriantutuianu01@gmail.com>
2024-06-24 22:51:18 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Homepage from "./components/Homepage/Home";
+import Homepage from "./components/homepage/Home";
 import NotFoundRoute from "./components/NotFoundRoute/NotFoundRoute";
 import CountryDetails from "./components/CountryDetails/CountryDetails";
 import Header from "./components/Header/Header";
