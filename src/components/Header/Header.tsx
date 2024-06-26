7ebfdb588d1232e7bbde3dfcf54921cb4fe3b2ed
Adrian Tut <adriantutuianu01@gmail.com>
2024-06-26 13:07:19 +0100

    Update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import "./header.css";
 const Header = () => {
   return (
     <header className="header flex justify-center items-center h-28 relative bg-gray-100">
-      <h1 className="text-5xl font-bold text-center">World Statistics</h1>
+      <h1 className="text-5xl font-bold text-center">World Statistics Hub</h1>
       <div className="absolute bottom-0 left-0 w-full h-0.5 bg-gray-500 opacity-75"></div>
     </header>
   );
