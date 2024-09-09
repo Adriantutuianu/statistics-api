30efaeb45da15ac5c963c9627bd8eabc1eaead32
Adrian Tut <adriantutuianu01@gmail.com>
2024-09-09 22:22:20 +0100

    Styled the table country details using next ui + tailwind

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getCountry } from "../../apiService";
-import "./countryDetails.css";
 import { FullCountry } from "../../types";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@nextui-org/table";
+import { Button } from "@nextui-org/button";
 
 const CountryDetails: React.FC = () => {
   let { countryCode } = useParams<{ countryCode: string }>();
@@ -16,7 +24,6 @@ const CountryDetails: React.FC = () => {
       if (countryCode) {
         try {
           const countryDetails = await getCountry(countryCode);
-          console.log(countryDetails);
           setCountry(countryDetails.data);
         } catch (error) {
           console.error("Error fetching country:", error);
@@ -31,76 +38,57 @@ const CountryDetails: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 flex justify-center items-center ">
-      <div className="w-full max-w-4xl bg-white shadow-md rounded px-8 pt-8 pb-8 mb-4">
-        <h1 className="text-center text-3xl font-bold mb-4">Country Details</h1>
-        <table className="w-full bg-white border border-gray-300">
-          <tbody>
-            <tr>
-              <td
-                className="py-4 px-4 border-b font-bold"
-                style={{ whiteSpace: "nowrap" }}
-              >
+    <div className="container mx-auto mt-[100px] p-4 flex justify-center items-center">
+      <div className="w-full md:w-3/4 lg:w-2/3 bg-white shadow-md rounded px-4 md:px-8 pt-8 pb-8 mb-4">
+        <h1 className="text-center text-3xl font-bold mb-6">Country Details</h1>
+        <Table
+          aria-label="Country Details"
+          className="w-full border border-gray-300"
+        >
+          <TableHeader>
+            <TableColumn className="border-b border-gray-300 pb-5 text-left text-xl">
+              Field
+            </TableColumn>
+            <TableColumn className="border-b border-gray-300 pb-5 text-left text-xl">
+              Value
+            </TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow className="border-b border-gray-300">
+              <TableCell className="border-r border-gray-300 p-2 text-lg">
                 Country Code
-              </td>
-              <td
-                className="py-4 px-4 border-b"
-                style={{ whiteSpace: "nowrap" }}
-              >
-                {countryCode}
-              </td>
-            </tr>
-            <tr>
-              <td
-                className="py-4 px-4 border-b font-bold"
-                style={{ whiteSpace: "nowrap" }}
-              >
+              </TableCell>
+              <TableCell className="p-2 text-lg">{countryCode}</TableCell>
+            </TableRow>
+            <TableRow className="border-b border-gray-300">
+              <TableCell className="border-r border-gray-300 p-2 text-lg">
                 Country Name
-              </td>
-              <td
-                className="py-4 px-4 border-b"
-                style={{ whiteSpace: "nowrap" }}
-              >
-                {country?.name}
-              </td>
-            </tr>
-            <tr>
-              <td
-                className="py-4 px-4 border-b font-bold"
-                style={{ whiteSpace: "nowrap" }}
-              >
+              </TableCell>
+              <TableCell className="p-2 text-lg">{country?.name}</TableCell>
+            </TableRow>
+            <TableRow className="border-b border-gray-300">
+              <TableCell className="border-r border-gray-300 p-2 text-lg">
                 Country Capital
-              </td>
-              <td
-                className="py-4 px-4 border-b"
-                style={{ whiteSpace: "nowrap" }}
-              >
-                {country?.capital}
-              </td>
-            </tr>
-            <tr>
-              <td
-                className="py-4 px-4 border-b font-bold"
-                style={{ whiteSpace: "nowrap" }}
-              >
+              </TableCell>
+              <TableCell className="p-2 text-lg">{country?.capital}</TableCell>
+            </TableRow>
+            <TableRow className="border-b border-gray-300">
+              <TableCell className="border-r border-gray-300 p-2 text-lg">
                 Country Currency Code
-              </td>
-              <td
-                className="py-4 px-4 border-b"
-                style={{ whiteSpace: "nowrap" }}
-              >
+              </TableCell>
+              <TableCell className="p-2 text-lg">
                 {country?.currencyCodes.join(", ")}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
         <div className="flex justify-center mt-8">
-          <button
+          <Button
+            className="bg-blue-500 text-white text-xl rounded-full px-4 py-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             onClick={handleGoHome}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
           >
             Go to Country Full List
-          </button>
+          </Button>
         </div>
       </div>
     </div>
